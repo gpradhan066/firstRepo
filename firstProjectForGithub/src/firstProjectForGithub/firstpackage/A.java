86d3db7b310f132d7ddbfd7d4fa72c9b497df237
gprad <gprad@192.168.2.109>
2023-06-26 23:51:59 +0530

    second commit

---
+++ firstProjectForGithub/src/firstProjectForGithub/firstpackage/A.java
@@ -0,0 +1,7 @@
+package firstProjectForGithub.firstpackage;
+
+public class A {
+public static void main(String[] args) {
+	System.out.println("gopinath pradhan");
+}
+}
--- firstProjectForGithub/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author gprad
- *
- */
-module firstProjectForGithub {
-}
